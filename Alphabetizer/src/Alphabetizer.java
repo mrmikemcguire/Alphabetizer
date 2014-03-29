4a8c251a13bf7f8f41f84fa59b6186c1812f6e79
mrmikemcguire <mcguire@mullenhigh.com>
2014-03-29 10:24:56 -0700

    Initial commit

---
+++ Alphabetizer/src/Alphabetizer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Alphabetizer
+	{
+
+	public static void main(String[] args) 
+		{
+		String [] name = new String [3];
+		Scanner scan = new Scanner (System.in);
+		
+		for (int i=1; i<4; i++) 
+			{
+			System.out.println("Enter name #" + i + ":");
+			name[i - 1] = scan.nextLine();
+			}
+		
+		java.util.Arrays.sort(name);
+		
+		System.out.println();
+		System.out.println("The names are ");
+		for (int i=0; i<name.length; i++)
+			{
+			System.out.println(name[i]);
+			}
+		}
+	}
